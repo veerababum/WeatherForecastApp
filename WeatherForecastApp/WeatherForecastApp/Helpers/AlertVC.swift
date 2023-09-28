a2af3532279c0e3a000503b56b83fae57c03f63c
Vmulugu1 <veerababu.mulugu1@t-mobile.com>
2023-09-28 11:08:23 -0500

    Add the preparation plan file to project

--- WeatherForecastApp/WeatherForecastApp/Helpers/AlertVC.swift
+++ WeatherForecastApp/WeatherForecastApp/Helpers/AlertVC.swift
@@ -6,18 +6,3 @@
 //
 
 import Foundation
-
-
-// 1.beeter UI
-//2. async await - combine
-//3. errror handiling
-//4. unit test cases
-//5. loading better
-//6. alerts messages
-//7.in session we can configure the time out
-// 8. if authorization is needed, its better to add
-//9.In terms of security, if we have otpion for SSL Pinning, we can add it
-//10. we can create blocks
-//11.another one is using List
-//app icon
-
